01d4e91da4c55fb35bdef05ee7db91e9f28a32b3
plhnk <paul.hanaoka@gmail.com>
2020-05-29 11:37:54 -0700

    feature: break Header component out

---
+++ src/components/Header/index.js
@@ -0,0 +1,25 @@
+import { Illustration } from './illustration'
+
+import React from 'react'
+
+import styles from './styles.module.scss'
+
+const Header = ({ props }) => {
+	return (
+		<div className={styles.TopArea}>
+			<Illustration />
+			<div className={styles.MainCall}>
+				<h1>María y Víctor</h1>
+				<p>
+					Queremos que seais parte de nuestra historia y para ello
+					necesitamos vuestra confirmación
+				</p>
+				<a target="_blank" href="https://google.com">
+					<button className={styles.boton}>CONFIRMAR</button>
+				</a>
+			</div>
+		</div>
+	)
+}
+
+export default Header
--- src/components/header.js
+++ src/components/header.js
@@ -3,16 +3,21 @@ import Link from 'gatsby-link'
 import './Header.css'
 
 const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="Illustration">
-      <img src={require('../images/illustration.png')} width="auto"/>
-    </div>
-    <div className="MainCall">
-      <h1>María and Víctor</h1>
-      <p>Queremos que seais parte de nuestra historia y para ello necesitamos vuestra confirmación</p>
-      <Link to="/Buy"><button>CONFIRMAR</button></Link>
-    </div>
-  </div>
+	<div className="Header">
+		<div className="Illustration">
+			<img src={require('../images/illustration.png')} width="auto" />
+		</div>
+		<div className="MainCall">
+			<h1>María and Víctor</h1>
+			<p>
+				Queremos que seais parte de nuestra historia y para ello
+				necesitamos vuestra confirmación
+			</p>
+			<Link to="/Buy">
+				<button>CONFIRMAR</button>
+			</Link>
+		</div>
+	</div>
 )
 
 export default Header
