ef54f45a24390cf85c11d6a5651b4609ed6ad56e
plhnk <paul.hanaoka@gmail.com>
2020-06-04 22:57:04 -0700

    sketch out StickyCTA component

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import Button from './Button'
 import Header from './Header'
 import Link from './Link'
 import FancyHr from './FancyHr'
+import StickyCTA from './StickyCTA'
 import Illustration from './Illustration'
 
-export { Button, FancyHr, Link, Header, Illustration }
+export { Button, FancyHr, Link, Header, Illustration, StickyCTA }
